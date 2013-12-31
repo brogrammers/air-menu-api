e21cf0443357522433c6e78d2f824a03de983a2b
tsov <tsov@me.com>
2013-12-31 17:58:24 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirMenuApi::Application.config.secret_token = '676aa28e6899b3fe7ac270ef5d1b0be605c308d0bd9dae130726dc526d43f2e52b2c3fb1aad0646b3a8dfae18f374f4a22a4d3da968ebe2d24e30f4d45b8928e'
