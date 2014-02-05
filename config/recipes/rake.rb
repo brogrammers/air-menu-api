fd07d93c7411309762b8b1b767a7debc74a456d2
Max Hoffmann <tsov@me.com>
2014-02-05 20:59:25 +0000

    changed deployment plan

--- config/recipes/rake.rb
+++ config/recipes/rake.rb
@@ -5,4 +5,4 @@ namespace :rake do
     run "cd #{current_path}; RAILS_ENV=production bundle exec rake apipie:static"
   end
 end
-after "deploy:assets:precompile", "rake:apipie"
+after "deploy:create_symlink", "rake:apipie"
