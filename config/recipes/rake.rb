86c4994c205c67f5f22436390ecf86f52cc93f3e
Max Hoffmann <tsov@me.com>
2014-02-05 19:57:05 +0000

    fixed issues with apipie cap task

--- config/recipes/rake.rb
+++ config/recipes/rake.rb
@@ -1,7 +1,7 @@
 namespace :rake do
   desc "Generate cache to avoid production dependencies on markup languages"
-  task :"apipie:cache", :roles => :app do
-    run("cd #{current_path}; rake apipie:cache")
+  task :"apipie", :roles => :app do
+    run "cd #{current_path}; rake apipie:cache"
   end
 end
 after "deploy:assets:precompile", "rake:apipie:cache"
