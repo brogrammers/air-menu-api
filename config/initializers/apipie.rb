25fbe2daaa3330d0cfa67a75aaff8547766d5be1
Max Hoffmann <tsov@me.com>
2014-02-05 20:57:07 +0000

    changed docs route

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,7 +3,7 @@ Apipie.configure do |config|
   config.copyright               = "&copy; #{Time.now.year} AirMenu"
   config.default_version         = "v1"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/api/docs"
+  config.doc_base_url            = "/docs"
   config.validate                = false
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v1/*.rb"
   config.app_info                = "AirMenu REST API documentation"
