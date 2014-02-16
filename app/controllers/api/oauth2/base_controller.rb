11897e224e210097518cdb9c2aa5d8cd0978042c
Max Hoffmann <tsov@me.com>
2014-02-16 20:18:13 +0000

    Added better documentation

--- app/controllers/api/oauth2/base_controller.rb
+++ app/controllers/api/oauth2/base_controller.rb
@@ -4,8 +4,7 @@ module Api
 
       resource_description do
         api_version 'oauth2'
-        app_info 'OAuth 2.0 Documentation. ' +
-                     'Getting Started'
+        app_info File.read("#{Rails.root}/public/docs/api/oauth2/desc.txt")
         api_base_url '/api/oauth2'
       end
 
--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,8 +4,7 @@ module Api
 
       resource_description do
         api_version 'v1'
-        app_info 'AirMenu Api v1 is the first API documentation by the AirMenu team. Find out how to use our System. ' +
-                 'Getting Started'
+        app_info File.read("#{Rails.root}/public/docs/api/v1/desc.txt")
         api_base_url '/api/v1'
       end
 
