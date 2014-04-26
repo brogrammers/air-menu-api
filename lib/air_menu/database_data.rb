2793ff6a5661de72af0336b11ea36106802d1dc5
Max Hoffmann <tsov@me.com>
2014-04-26 14:21:33 +0100

    fix for encodings

--- lib/air_menu/database_data.rb
+++ lib/air_menu/database_data.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require 'json'
 require 'pp'
 
@@ -292,7 +293,7 @@ DATABASE_TABLES = {
   :restaurant => [
       {
           :name => 'The Church',
-          :description => 'St. Mary’s closed in 1964 and lay derelict for a number of years until it was purchased by John Keating in 1997. Following extensive restoration over a seven year period, this List 1 building finally re-opened its doors in December 2005 as John M. Keating’s Bar. The tasteful conversion and refurbishment of this Dublin landmark was acknowledged at the Dublin City Neighbourhood Awards 2006, where it won first prize in the category of Best Old Building In September 2007 the building was acquired by new owners and renamed “The Church Bar & Restaurant” and its range of services was expanded to include a Café, Night Club and a Barbeque area on the terrace.',
+          :description => 'St. Mary’s closed in 1964 and lay derelict for a number of years until it was purchased by John Keating in 1997. Following extensive restoration over a seven year period, this List 1 building finally re-opened its doors in December 2005 as John M. Keating’s Bar. The tasteful conversion and refurbishment of this Dublin landmark was acknowledged at the Dublin City Neighbourhood Awards 2006, where it won first prize in the category of Best Old Building In September 2007 the building was acquired by new owners and renamed "The Church Bar & Restaurant" and its range of services was expanded to include a Cafe, Night Club and a Barbeque area on the terrace.',
           :loyalty => false,
           :remote_order => false,
           :conversion_rate => 0.0,
